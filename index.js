22a12cdb6733e899d03a113b3e97191365577f69
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2023-07-08 23:51:47 +0530

    Added initial code,

---
+++ index.js
@@ -0,0 +1,5 @@
+// npm init -- create node project --
+// npm i nodemon (continuously run node)
+// nodemon <filename> (to run node locally)
+const message = 'with backend...'
+console.log('Hello world!', message)
